018c9dfa740c661277298039cbf56d7261f90e6f
ip-123 <inespereira1400@gmail.com>
2024-12-17 12:39:23 +0000

    Adicionei classe categoria + tarefa

---
+++ src/Tarefa.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Tarefa {
+    private int id;
+    private Date prazo;
+    private String nome;
+    private Categoria categoria;
+
+}
---
+++ src/TarefaCategoria.java
@@ -0,0 +1,4 @@
+public class TarefaCategoria {
+    private int idTarefa;
+    private int idCategoria;
+}

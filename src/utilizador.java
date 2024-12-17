c88b0e68592d8a0fc970685ac17d1cd5b8ac4662
sofiapeixoto <sofiapeixoto54@gmail.com>
2024-12-17 12:40:37 +0000

    inicio

---
+++ src/utilizador.java
@@ -0,0 +1,11 @@
+public class utilizador {
+    private String nome;
+
+    public String setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
